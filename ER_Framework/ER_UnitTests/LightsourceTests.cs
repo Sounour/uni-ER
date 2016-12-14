a45b5ae0773230428d46ef62d6eb7c40f80b2221
Sebastian Osterbrink <Sounour@gmail.com>
2016-12-14 03:56:04 +0100

    Added Unit tests

---
+++ ER_Framework/ER_UnitTests/LightsourceTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace de.sounour.uni.er.Tests
+{
+    [TestClass]
+    public class LightsourceTests
+    {
+        private readonly double tolerance = 0.1;
+
+        [TestMethod]
+        public void CalculateBrightnessTest()
+        {
+            var w = new World(200, 200);
+            var l = new Lightsource(w, 10, 10, 20, 20);
+            Assert.AreEqual(20, l.CalculateBrightness(10, 10), tolerance);
+            Assert.AreEqual(0, l.CalculateBrightness(100, 100), tolerance);
+            Assert.AreEqual(10, l.CalculateBrightness(0, 10), tolerance);
+            Assert.AreEqual(10, l.CalculateBrightness(10, 0), tolerance);
+            Assert.AreEqual(20 - 10*Math.Sqrt(2),
+                l.CalculateBrightness(0, 0), tolerance
+            );
+        }
+    }
+}
---
+++ ER_Framework/ER_UnitTests/RobotTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using de.sounour.uni.er;
+
+namespace de.sounour.uni.er.Tests
+{
+    [TestClass()]
+    public class RobotTest
+    {
+        private double tolerance = 0.1;
+
+        [TestMethod()]
+        public void TurnTest()
+        {
+            World w = new World(200, 200);
+            Robot r = new Robot(w);
+            Assert.AreEqual(r.Heading , new Vector(1,0));
+            r.Turn(90);
+            Assert.IsTrue(Math.Abs(r.Heading.X) < tolerance);
+            Assert.IsTrue(Math.Abs(r.Heading.Y - 1 ) < tolerance);
+
+            r.Turn(90);
+            Assert.IsTrue(Math.Abs(r.Heading.X +1 ) < tolerance);
+            Assert.IsTrue(Math.Abs(r.Heading.Y) < tolerance);
+        }
+
+        [TestMethod]
+        public void TestMove()
+        {
+            World w = new World(200, 200);
+            Robot r = new Robot(w);
+            Assert.AreEqual(r.PositionX, 0);
+            Assert.AreEqual(r.PositionY, 0);
+            r.MoveStep();
+            Assert.IsTrue(Math.Abs(r.PositionX) - 2 < tolerance);
+            Assert.IsTrue(Math.Abs(r.PositionY) < tolerance);
+
+        }
+
+
+    }
+}
