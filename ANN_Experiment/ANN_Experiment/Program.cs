c5d8171acbc044ab89365eb5f8b445033b3f156a
Sebastian Osterbrink <Sounour@gmail.com>
2017-02-28 00:09:12 +0100

    Added new files + Moved the random functionality to the randoms class

--- ANN_Experiment/ANN_Experiment/Program.cs
+++ ANN_Experiment/ANN_Experiment/Program.cs
@@ -1,18 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ANN_Experiment
 {
-    class Program
+    internal class Program
     {
-        public static Random Random; 
+        private static List<Network> population;
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Random = new Random();
+            InitializePopulation(GlobalVars.PopulationSize);
+
+            var inputBools = new List<bool[]>
+            {
+                new[] {false, false},
+                new[] {true, false},
+                new[] {false, true},
+                new[] {true, true}
+            };
+
+
+            for (int generation = 0; generation < GlobalVars.Generations; generation++)
+            {
+                Console.Out.WriteLine("Starting selection for genereation {0}", generation);
+
+                SortedList<double, Network> ranking = new SortedList<Double,Network>(); 
+                foreach (Network instance in population)
+                {
+                    ranking.Add(instance.Fitness(inputBools),instance);
+                }
+
+                List<Network> newGeneration = new List<Network>();
+                int numberOfElites = (int) Math.Round(GlobalVars.Generations*GlobalVars.Elites); 
+                newGeneration.AddRange(ranking.Values.Take(numberOfElites));
+
+                foreach (var nonSelectedNetwork in ranking.Skip(numberOfElites))
+                {
+                    newGeneration.Add(nonSelectedNetwork.Mutate());
+
+                }
+
+                population = newGeneration;
+
+            }
+            
+            
+           
+            Console.ReadLine();
+        }
+
+        private static void InitializePopulation(int i)
+        {
+            population = new List<Network>();
+            for (int j = 0; j < i; j++)
+                population.Add(new Network(2, new[] {2}));
         }
     }
 }
---
+++ ANN_Experiment/ANN_Experiment/Randoms.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ANN_Experiment
+{
+    internal class Randoms
+    {
+        private static readonly Random Random = new Random();
+
+        public static double RandomDouble(double max, double min = 0)
+        {
+            return min + Random.NextDouble()*(max - min);
+        }
+
+
+        public static bool RandomBool(double probability = 0.4)
+        {
+            return Random.NextDouble() <= probability;
+        }
+    }
+}
