a93a39a2791aab6bd5fd3080a4d9df0f43c09bc6
Sebastian Osterbrink <Sounour@gmail.com>
2017-02-27 13:48:16 +0100

    New files for ANNs

---
+++ ANN_Experiment/ANN_Experiment/Neuron.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace ANN_Experiment
+{
+    internal class Neuron
+    {
+        private readonly Func<double, double> activationFunction;
+
+        // w_0 to w_n with possible additional w_bias at the end 
+        private readonly double[] weights;
+
+
+        public Neuron(Func<double, double> activationFunction, int numberOfInputs)
+        {
+            this.activationFunction = activationFunction;
+
+            weights = new double[numberOfInputs];
+            for (int i = 0; i < numberOfInputs; i++)
+                weights[i] = (Program.Random.NextDouble() - 0.5)*2.0;
+        }
+
+        public double Output(double[] inputs)
+        {
+            if ((inputs.Length > weights.Length) || (inputs.Length + 1 < weights.Length))
+                return double.NaN;
+
+            double net = inputs.Length == weights.Length ? 0 : weights.Last();
+            for (int i = 0; i < inputs.Length; i++)
+                net += inputs[i]*weights[i];
+
+            return activationFunction(net);
+        }
+
+        public void mutate()
+        {
+            for (int i = 0; i < weights.Length; i++)
+            {
+            }
+        }
+    }
+}
---
+++ ANN_Experiment/ANN_Experiment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ANN_Experiment
+{
+    class Program
+    {
+        public static Random Random; 
+
+        static void Main(string[] args)
+        {
+            Random = new Random();
+        }
+    }
+}
