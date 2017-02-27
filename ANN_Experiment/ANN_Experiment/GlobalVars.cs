99bfce0804135ae8ec6d63762e3ed83575c1d20a
Sebastian Osterbrink <Sounour@gmail.com>
2017-02-28 00:07:47 +0100

    Added global variables

---
+++ ANN_Experiment/ANN_Experiment/GlobalVars.cs
@@ -0,0 +1,15 @@
+﻿namespace ANN_Experiment
+{
+    internal class GlobalVars
+    {
+        public static double MaxMutation { get; } = 0.5;
+
+        public static double MutationProbailiy { get; } = 0.3;
+
+        public static double Elites { get; } = 0.2;
+
+        public static int PopulationSize { get; } = 50;
+
+        public static int Generations { get; } = 20;
+    }
+}
