3a4fed4a1881c55afb678d4ac4d695c3a723cdd7
Sebastian Osterbrink <Sounour@gmail.com>
2017-03-06 18:26:10 +0100

    Improved Calculation

--- ANN_Experiment/ANN_Experiment/Program.cs
+++ ANN_Experiment/ANN_Experiment/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Data;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace ANN_Experiment
@@ -11,12 +8,13 @@ namespace ANN_Experiment
     internal class Program
     {
         private static List<Network> population;
+        private static List<bool[]> inputBools;
 
         private static void Main(string[] args)
         {
             InitializePopulation(GlobalVars.PopulationSize);
 
-            var inputBools = new List<bool[]>
+            inputBools = new List<bool[]>
             {
                 new[] {false, false},
                 new[] {true, false},
@@ -24,39 +22,107 @@ namespace ANN_Experiment
                 new[] {true, true}
             };
 
+            perfectXor();
 
+             mutateAndEvolve();
+        }
+
+        public static void perfectXor()
+        {
+            Network n = new Network(2, new[] {2});
+            n.HiddenLayers[0][0].Bias = 0.0; 
+            n.HiddenLayers[0][0].Weights = new[] {1.0, -1.0 };
+
+            n.HiddenLayers[0][1].Bias = 0.0;
+            n.HiddenLayers[0][1].Weights = new[] {1.0, -1.0};
+
+            n.OutputNeuron.Bias = 0.0; 
+            n.OutputNeuron.Weights = new[] { 1.0 , 1.0};
+
+            Console.Out.WriteLine(n.HiddenLayers[0][1].Output(new []{ 0.0, 0.0}));
+            Console.Out.WriteLine(n.HiddenLayers[0][1].Output(new[] { 1.0, 0.0 }));
+            Console.Out.WriteLine(n.HiddenLayers[0][1].Output(new[] { 0.0, 1.0 }));
+            Console.Out.WriteLine(n.HiddenLayers[0][1].Output(new[] { 1.0, 1.0 }));
+
+
+
+            Console.Out.WriteLine("-------");
+
+            Console.Out.WriteLine(n.Fitness(inputBools));
+
+            Console.Out.WriteLine("-------"); 
+            foreach (var input in inputBools)
+                Console.Out.WriteLine(n.GetValue(input));
+
+            Console.ReadLine();
+
+        }
+
+        public static void mutateAndEvolve()
+        {
             StreamWriter outWriter = new StreamWriter("log.csv");
             DateTime Started = DateTime.Now;
+
+            foreach (var network in population)
+            {
+                network.Fitness(inputBools); 
+            }
+
             for (int generation = 0; generation < GlobalVars.Generations; generation++)
             {
-                if (generation%500 == 0)
+                if (generation%(GlobalVars.Generations /100) == 0)
                 {
-                    Console.Out.WriteLine("Starting selection for genereation {0}, ETA: {1} minutes", generation, (DateTime.Now - Started).TotalMinutes / generation  * (GlobalVars.Generations - generation));
+                    Console.Out.WriteLine("Starting selection for genereation {0}, ETA: {1} minutes", generation,
+                        (DateTime.Now - Started).TotalMinutes / generation*(GlobalVars.Generations - generation));
                     outWriter.Write(generation);
-                    foreach (var network in population)
-                    {
-                        outWriter.Write(",{0}", network.Fitness(inputBools));
-                    }
+                    foreach (Network network in population)
+                        outWriter.Write(" {0}", network.Fitness(inputBools));
                     outWriter.WriteLine();
                     outWriter.Flush();
                 }
 
-                List<Network> newGeneration = new List<Network>();
                 foreach (var network in population)
                 {
+                    if (network.LastFitnessValue > 0.99)
+                    {
+                        Console.Out.WriteLine(network);
+                        Console.ReadLine();
+                        return; 
+                    }
+                }
+                
+                var newGeneration = new List<Network>();
+                Parallel.ForEach(population, network =>
+                {
                     Network newNetwork = network.Mutate();
-                    double newFitness = newNetwork.Fitness(inputBools);
-                    double oldFitness = network.Fitness(inputBools);
-                    newGeneration.Add(newFitness > oldFitness ? newNetwork : network);
-                }                
-                population = newGeneration;
+                    newNetwork.Fitness(inputBools);
 
+                    int max = 0;
+                    while (newNetwork.LastFitnessValue < network.LastFitnessValue && max < 1000)
+                    {
+                        max++; 
+                        newNetwork =  network.Mutate();
+                        newNetwork.Fitness(inputBools);
+                    }
+
+                    if (newNetwork.LastFitnessValue > network.LastFitnessValue)
+                    {
+                        Console.Out.WriteLine("{0} -> {1}", network.LastFitnessValue, newNetwork.LastFitnessValue);
+                        //  Console.Out.WriteLine(newNetwork.ToString());
+                    }
+
+                    lock (newGeneration)
+                    {
+                        newGeneration.Add(newNetwork.LastFitnessValue > network.LastFitnessValue ? newNetwork : network);
+                    }
+
+                }); 
+
+                population = newGeneration;
             }
-            
+
             foreach (Network network in population)
-            {
                 Console.Out.WriteLine("Fitness: {0}", network.Fitness(inputBools));
-            }
 
             Console.ReadLine();
         }
