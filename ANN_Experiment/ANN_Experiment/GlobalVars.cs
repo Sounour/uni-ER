17a745aa95a2ce656efb6121d058a50112d9b29b
Sebastian Osterbrink <Sounour@gmail.com>
2017-03-06 18:24:50 +0100

    changed values

--- ANN_Experiment/ANN_Experiment/GlobalVars.cs
+++ ANN_Experiment/ANN_Experiment/GlobalVars.cs
@@ -2,16 +2,16 @@
 {
     internal class GlobalVars
     {
-        public static double MaxMutation { get; } = 0.2;
+        public static double MaxMutation { get; } = 2;
 
-        public static double NeuronMutationProbailiy { get; } = 0.3;
+        public static double NeuronMutationProbailiy { get; } = 0.5;
 
         public static double Elites { get; } = 0.0;
 
-        public static int PopulationSize { get; } = 2000;
+        public static int PopulationSize { get; } = 10;
 
-        public static int Generations { get; } = 100000;
+        public static int Generations { get; } = 3000;
 
-        public static double NeuronValueMutationProbailiy { get; set; } = 0.3;
+        public static double NeuronValueMutationProbailiy { get; set; } = 0.9;
     }
 }
